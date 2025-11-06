710f3b011c8d3b35f6769ba9d065d489321388b6
MamyrAltaibek <139006850+MMR000@users.noreply.github.com>
2025-11-06 23:59:06 +0500

    for paper

--- app.py
+++ app.py
@@ -7,28 +7,27 @@ from utils.paths import PathManager
 from celery_app import start_rabbitmq_listener
 
 
-# 初始化 Flask 应用
+# Initialize the Flask application
 app = Flask(
     __name__,
     static_folder='static',
     template_folder='templates'
 )
 
-# 配置文件夹路径
-app.config['UPLOAD_FOLDER'] = os.path.join(app.static_folder, 'audio')  # 上传音频文件夹
-app.config['VIDEO_OUTPUT_FOLDER'] = os.path.join(app.static_folder, 'video_output')  # 视频输出文件夹
-app.config['TEMPLATE_FOLDER'] = os.path.join(app.static_folder, 'video_templates') # 视频模板文件夹
+# Configuration folder path
+app.config['UPLOAD_FOLDER'] = os.path.join(app.static_folder, 'audio')  
+app.config['VIDEO_OUTPUT_FOLDER'] = os.path.join(app.static_folder, 'video_output')  
+app.config['TEMPLATE_FOLDER'] = os.path.join(app.static_folder, 'video_templates') 
 
-# 初始化路径管理器
+# Initialize the path manager
 path_manager = PathManager(app)
-path_manager.ensure_directories()  # 确保必要目录存在
+path_manager.ensure_directories()
 
-# 注册蓝图（Blueprint）
-app.register_blueprint(home_bp)  # 主页蓝图
-app.register_blueprint(text_processing_bp)  # 文本处理蓝图
-app.register_blueprint(video_generation_bp)  # 视频生成蓝图
+# Register a Blueprint
+app.register_blueprint(home_bp)
+app.register_blueprint(text_processing_bp)
+app.register_blueprint(video_generation_bp)
 
-# 启动 Flask 服务器
 if __name__ == '__main__':
     start_rabbitmq_listener()
     app.run(debug=True, host='0.0.0.0', port=5000)
